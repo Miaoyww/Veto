6d6548800040104d636fd1213ea8fd7ad80f8404
Miaoyww <miaomiaoywww@gmail.com>
2026-04-03 23:13:34 +0800

    feat: 添加 GitHub Pages 部署工作流，更新依赖项并切换到静态适配器

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
@@ -8,9 +7,6 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
-	]
+		svg()
+	].filter(Boolean)
 });
