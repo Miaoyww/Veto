e2e6830aad317d918ec1e99d74d3ca4e5aa6ff5b
Miaoyww <miaomiaoywww@gmail.com>
2026-04-05 11:47:31 +0800

    feat: 添加模组和通用选项卡到设置对话框，优化用户导航体验

--- src/lib/components/dialog/settings/crisis/settings-dialog.svelte
+++ src/lib/components/dialog/settings/crisis/settings-dialog.svelte
@@ -7,6 +7,8 @@
 	import { fade, scale } from 'svelte/transition';
 	import { X } from '@lucide/svelte';
 	import { ScrollArea } from '$lib/components/ui/scroll-area';
+	import Mods from '$lib/components/settings/pages/mods.svelte';
+	import General from '$lib/components/settings/pages/general.svelte';
 
 	let activeTab = $state('about'); // 控制当前选中 tab
 
@@ -21,15 +23,23 @@
 		transition:fade={{ duration: 150 }}
 	>
 		<!-- 左侧边栏 -->
-		<Tabs.Tabs bind:value={activeTab} orientation="vertical" class="flex h-full bg-background/75 backdrop-blur-md rounded-lg">
+		<Tabs.Tabs
+			bind:value={activeTab}
+			orientation="vertical"
+			class="flex h-full rounded-lg bg-background/75 backdrop-blur-md"
+		>
 			<Tabs.List>
 				<Tabs.Trigger value="battle" onclick={() => switchTab('battle')}>战局</Tabs.Trigger>
+				<Tabs.Trigger value="mod" onclick={() => switchTab('mod')}>模组</Tabs.Trigger>
+				<Tabs.Trigger value="general" onclick={() => switchTab('general')}>通用</Tabs.Trigger>
 				<Tabs.Trigger value="about" onclick={() => switchTab('about')}>关于</Tabs.Trigger>
 			</Tabs.List>
 		</Tabs.Tabs>
 
 		<!-- 页面内容 -->
-		<div class="bg-background/75 backdrop-blur-md ml-6 flex h-200 w-240 flex-1 flex-col rounded-lg p-6">
+		<div
+			class="ml-6 flex h-200 w-240 flex-1 flex-col rounded-lg bg-background/75 p-6 backdrop-blur-md"
+		>
 			<Button
 				variant="ghost"
 				size="icon"
@@ -41,8 +51,10 @@
 
 			<div>
 				<ScrollArea class="mt-5 h-180 w-full" scrollbarYClasses="hidden">
-					{#if activeTab === 'about'}<About />{/if}
 					{#if activeTab === 'battle'}<Battle />{/if}
+					{#if activeTab === 'mod'}<Mods />{/if}
+					{#if activeTab === 'general'}<General />{/if}
+					{#if activeTab === 'about'}<About />{/if}
 				</ScrollArea>
 			</div>
 		</div>
