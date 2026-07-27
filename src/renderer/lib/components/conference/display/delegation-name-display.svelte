67350e567d907918c8bc7385cce8085fb1a03e46
Miaoyww <miaomiaoywww@gmail.com>
2026-07-27 09:23:48 +0800

    feat: 点名使用AutoFitText

--- src/renderer/lib/components/conference/display/delegation-name-display.svelte
+++ src/renderer/lib/components/conference/display/delegation-name-display.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import AutoFitText from './auto-fit-text.svelte'
+
   /**
    * delegation-name-display.svelte
    * ──────────────────────────────
@@ -18,8 +20,12 @@
 </script>
 
 <div class={containerClass}>
-  <div class="text-9xl font-semibold tracking-wide text-white">{name}</div>
+  <div class="text-9xl font-semibold tracking-wide text-white">
+    <AutoFitText text={name} />
+  </div>
   {#if shortName}
-    <div class="mt-5 text-3xl font-light tracking-[0.06em] text-white/30">{shortName}</div>
+    <div class="mt-5 text-3xl font-light tracking-[0.06em] text-white/30">
+      <AutoFitText text={shortName} />
+    </div>
   {/if}
 </div>
