aea93bdbb993241f3bf1f8b447bad5f7f1f92f29
Miaoyww <miaomiaoywww@gmail.com>
2026-08-04 19:15:47 +0800

    feat: step1

--- apps/frontend/env.d.ts
+++ apps/frontend/env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-/// <reference path="../preload/index.d.ts" />
+/// <reference path="../../apps/electron/preload/index.d.ts" />
